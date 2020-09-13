506d9574c811c850132c4088b2a5308bb988ffa7
Andrew Montalenti <andrew@parsely.com>
2020-09-13 13:55:39 -0400

    Add the find-queens function

--- main.clj
+++ main.clj
@@ -3,12 +3,12 @@
 (defn initial-board []
  ; eight queens on a board
  [ \- \- \- \- \- \- \- \-
-   \- \- \- \- \- \- \- \- 
    \- \- \- \- \- \- \- \-
    \- \- \- \- \- \- \- \-
    \- \- \- \- \- \- \- \-
    \- \- \- \- \- \- \- \-
-   \- \- \- \- \- \- \- \- 
+   \- \- \- \- \- \- \- \-
+   \- \- \- \- \- \- \- \-
    \Q \Q \Q \Q \Q \Q \Q \Q])
 
 (defn lookup [board pos]
@@ -21,3 +21,21 @@
 
 (lookup (initial-board) "a1")
 ;; => \Q
+
+(defn print-board [board]
+  (dorun (map println (partition 8 board))))
+
+(def board (initial-board))
+
+(println "Initial chessboard state:")
+(println "-------------------------")
+(print-board board)
+
+(defn find-queens [board]
+ (doseq [cell (map-indexed vector board)]
+   (if (= (nth cell 1) \Q)
+     (prn cell))))
+
+(println "Location of the queens:")
+(println "-----------------------")
+(find-queens board)
